a0c6de30d757ab3f74e2aaed38488e4e5140af28
mehrojalieev <aliyevmehrojbek8006@gmail.com>
2025-01-04 14:56:57 +0500

    I deployed to the server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose')
 require('dotenv').config();
+const cors = require("cors")
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors())
 const PORT = process.env.PORT || 4545
 
 const connectDB = async () => {
