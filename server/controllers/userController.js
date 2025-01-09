7b0cbb8bf6d74c212af793954cbbe9c38e3b589e
mehrojalieev <aliyevmehrojbek8006@gmail.com>
2025-01-09 11:26:11 +0500

    Mehrojbek added the controllers and Joi validation

---
+++ server/controllers/userController.js
@@ -0,0 +1,38 @@
+const { UserSchema } = require("../models/user.model")
+
+exports.getAllUers = async (req, res) => {
+    try {
+        const users = await UserSchema.find();
+        res.status(200).json(users);
+    } 
+    catch (error) {
+        res.status(500).json({message: error.message})    
+    }
+}
+
+
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await UserSchema.findById(req.params.id);
+
+        if(!user) return res.status(404).json({message: 'User not found'});
+        res.status(200).json(user);
+    } 
+    catch (error) {
+        res.status(500).json({message: error.message})    
+    }
+}
+
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const deletedUser = await UserSchema.findByIdAndDelete(req.params.id);
+        if(!deletedUser) return res.status(404).json({message: 'User not found'});
+
+        res.status(200).json({message: "User deleted successfully!", user: deletedUser})
+    } 
+    catch (error) {
+      res.status(500).json({message: error.message})    
+        
+    }
+}
---
+++ server/validations/authValidation.js
@@ -0,0 +1,15 @@
+const Joi = require('joi')
+
+const registerValidation = Joi.object({
+    firstname: Joi.string().min(3).required(),
+    lastname: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required()
+});
+
+const loginValidation = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required()
+})
+
+module.exports = {registerValidation, loginValidation}
