adf0759d1ca70b106b0a76c4f2a97f2fa9eed850
mehrojalieev <aliyevmehrojbek8006@gmail.com>
2025-01-15 10:33:59 +0500

    I added controller for productRoute

---
+++ server/controllers/productController.js
@@ -0,0 +1,74 @@
+const ProductSchema = require("../models/product.model")
+const  mongoose  = require("mongoose")
+
+const allProducts = async (req, res) => {
+    try {
+        const products = await ProductSchema.find()
+        res.status(200).json(products)
+    } 
+    catch (error) {
+        return res.status(500).json({message: error.message})    
+    }
+}
+
+const productById = async (req, res) => {
+    try {
+        const singleProduct = await ProductSchema.findById(req.params.id);
+
+        if(!singleProduct){
+            return res.status(404).json({message: 'Product not found!'})
+        }
+
+        res.status(200).json(singleProduct)
+    } 
+    catch (error) {
+        res.status(500).json({message: error.message})    
+    }
+}
+
+
+const deleteProduct = async (req, res) => {
+    const productId = req.params.id;
+
+    if(!mongoose.Types.ObjectId.isValid(productId)){
+        return res.status(400).json({message: 'Invalid Product ID!'})
+    }
+
+    try {
+        const deletedProduct = await ProductSchema.findOneAndDelete({_id: productId});
+
+        if(!deletedProduct){
+            res.status(404).json({message: 'Product not found!'})
+        }
+
+        res.status(200).json({
+            message: `Product with ID ${deletedProduct._id} deleted successfully!`,
+            product: deletedProduct
+        })
+
+    }
+     catch (error) {
+        res.status(500).json({message: error.message})    
+    }
+}
+
+
+const updateProduct = async (req, res) => {
+    try { 
+        const updatedProduct = await ProductSchema.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        if(!updatedProduct){
+            return res.status(404).json({message: "Product not found"})
+        }
+        res.status(200).json({
+            status: 200,
+            message: "Product updated successfully!",
+            data: updatedProduct
+        })
+    } 
+    catch (error) {
+        res.status(500).json({message: error.message})    
+    }
+}
+
+
+module.exports = {allProducts, productById, updateProduct, deleteProduct}
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,80 +1,13 @@
 const express = require("express")
-const ProductSchema = require("../models/product.model")
-const  mongoose  = require("mongoose")
 const verify_admin = require("../middleware/verify-admin")
-
+const { deleteProduct, updateProduct, productById, allProducts } = require("../controllers/productController")
 
 const router = express.Router()
 
 
-router.get('/all', async (req, res) => {
-        try {
-            const productData = await ProductSchema.find()
-            res.status(200).json(productData)
-        } 
-        catch (error) {
-            res.status(500).json({message: error.message})   
-        }
-})
-
-router.get('/:id', async (req, res) => {
-    try {
-        const singleProduct = await ProductSchema.findById(req.params.id);
-
-        if(!singleProduct){
-            return res.status(404).json({message: 'Product not found!'})
-        }
-
-        res.status(200).json(singleProduct)
-    } 
-    catch (error) {
-        res.status(500).json({message: error.message})    
-    }
-})
-
-
-router.put("/update/:id", verify_admin, async (req, res) => {
-    try { 
-        const updatedProduct = await ProductSchema.findByIdAndUpdate(req.params.id, req.body, {new: true});
-        if(!updatedProduct){
-            return res.status(404).json({message: "Product not found"})
-        }
-        res.status(200).json({
-            status: 200,
-            message: "Product updated successfully!",
-            data: updatedProduct
-        })
-    } 
-    catch (error) {
-        res.status(500).json({message: error.message})    
-    }
-})
-
-
-router.delete('/delete/:id', verify_admin, async (req, res) => {
-    const productId = req.params.id;
-
-    if(!mongoose.Types.ObjectId.isValid(productId)){
-        return res.status(400).json({message: 'Invalid Product ID!'})
-    }
-
-    try {
-        const deletedProduct = await ProductSchema.findOneAndDelete({_id: productId});
-
-        if(!deletedProduct){
-            res.status(404).json({message: 'Product not found!'})
-        }
-
-        res.status(200).json({
-            message: `Product with ID ${deletedProduct._id} deleted successfully!`,
-            product: deletedProduct
-        })
-
-    }
-     catch (error) {
-        res.status(500).json({message: error.message})    
-    }
-})
-
+router.get('/all', allProducts)
+router.get('/:id', productById)
+router.put("/update/:id", verify_admin, updateProduct)
+router.delete('/delete/:id', verify_admin, deleteProduct)
 
 module.exports = router
