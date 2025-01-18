fc78f3324651be59c50b060d7d6a5a0015100c10
mehrojalieev <aliyevmehrojbek8006@gmail.com>
2025-01-18 12:26:53 +0500

    I added several customHooks: useStorage and useNetworkStatus

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,14 @@
 import { RouterView } from 'vue-router';
 import Navbar from './layout/Navbar.vue';
 import {NNotificationProvider} from 'naive-ui'
+import { useNetworkStatus } from './hooks/useNetworkStatus';
+
+useNetworkStatus((status) => {
+    const message = `Your network is now ${status}.`;
+    console.log(message);
+   
+});
+
 </script>
 
 <template>
