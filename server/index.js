fc169c80f91880c362042d2741df3b8763da5db5
mehrojalieev <aliyevmehrojbek8006@gmail.com>
2025-01-30 22:53:08 +0500

    bug fix

--- server/index.js
+++ server/index.js
@@ -12,18 +12,18 @@ const PORT = process.env.PORT || 4545
 
 const connectDB = async () => {
     try {
-            await mongoose.connect(process.env.MONGODB_CONNECT_URL, {
-                useNewUrlParser: true,
+        await mongoose.connect(process.env.MONGODB_CONNECT_URL, {
+            useNewUrlParser: true,
             useUnifiedTopology: true,
             serverSelectionTimeoutMS: 5000,
-            }),
+        }),
             console.log('MongoDB connected !');
-            
-    } 
+
+    }
     catch (error) {
         console.error('Mongo connection error:', error);
-            process.exit(1)
-    }   
+        process.exit(1)
+    }
 }
 
 connectDB();
@@ -36,7 +36,7 @@ db.once('open', () => console.log('Connected to MongoDB !'));
 app.use('/swagger/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 
-app.use("/api/product",  require('./routes/productRoutes'));
+app.use("/api/product", require('./routes/productRoutes'));
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/user', require('./routes/userRoutes'));
 
